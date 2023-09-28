6dacc8816ea0a930915066f70141e8d4499d7494
Johan le stickman <Johan_Stickman@proton.me>
2023-09-28 20:40:47 +0200

    oe j'ai tt refait presque

--- index.js
+++ index.js
@@ -1,31 +1,65 @@
-const { FreeboxRegister } = require("freebox");
-const inquirer = require("inquirer");
+#!/usr/bin/env node
 
+var { RegisterFreebox } = require("freebox-wrapper")
+const { prompt } = require("inquirer")
+const { hostname } = require("os")
+const fetch = require("node-fetch")
+const serverDomain = process.argv[2] || "https://api-freeboxnotifier.vercel.app"
+console.log(serverDomain)
 async function main() {
+	// Afficher le message d'accueil
+	console.log("Bienvenue dans l'assistant de configuration de Freebox Call Notifier.")
+	console.log("Cet assistant vous aidera à authentifier notre service auprès de votre Freebox. Vous devrez être connecté au réseau WiFi de votre Freebox pour continuer.")
+	console.log("\x1b[33m%s\x1b[0m", "Un message sera affiché sur l'écran du Server pour vous demander de confirmer l'authentification.\n")
 
-	// Demander le @ telegram de l'utilisateur
-	const username = await inquirer.prompt([
+	// Demander à l'utilisateur de confirmer
+	const { confirm } = await prompt([
 		{
-			type: "input",
-			name: "telegram",
-			message: "Veuillez entrer votre @ telegram",
-		}, 
-	]);
+			type: "confirm",
+			name: "confirm",
+			message: "Voulez-vous continuer ?"
+		}
+	])
 
+	// Si l'utilisateur ne veut pas continuer, on quitte
+	if(!confirm) process.exit(0)
 
-	const freeboxRegister = new FreeboxRegister({
-		app_id: "fbx.telegram_notifier",
-		app_name: "Call Notifier",
-		app_version: "1.0.0",
-		device_name: "Telegram",
-	});
+	// Créer une instance de RegisterFreebox
+	const register = await RegisterFreebox({
+		showLogs: true, // affiche les logs dans la console, true par défaut
+		appId: "fbx.notifier",
+		appName: "Call Notifier",
+		appVersion: "1.0.0",
+		deviceName: hostname() || "le CLI"
+	})
 
-	console.log("Regardez votre freebox un message est apparu !")
-	console.log("Veuillez accepter la demande d'authentification sur votre box.")
+	// On vérifie les informations
+	if(!register.appToken || !register.apiDomain || !register.httpsPort){
+		console.log(`Impossible de récupérer les informations de connexion.${register.msg ? ` Message d'erreur : ${register.msg}` : " Veuillez réessayer."}`)
+		return process.exit(1)
+	}
 
-	const access = freeboxRegister.register();
+	// Log
+	console.log("Envoi des informations au serveur de Call Notifier...")
 
-	return username.telegram, access.app_token, access.app_id, access.api_domain, access.https_port, access.api_base_url, access.api_version;
+	// Demander un code d'association à l'API
+	var code = await fetch(`${serverDomain}/associateBoxWithTelegram`, {
+		method: "POST",
+		body: JSON.stringify(register.appToken, register.apiDomain, register.httpsPort),
+		headers: { "Content-Type": "application/json" }
+	}).then(res => res.text())
 
+	// On tente de parser en JSON
+	try { code = JSON.parse(code) }
+	catch(e) {
+		console.log("Impossible de récupérer la réponse du serveur distant, celui-ci est peut-être indisponible. Réessayez plus tard.")
+	}
+
+	// Si on a une erreur, on l'affiche
+	if(!code?.success || !code?.code) console.log("Une erreur est survenue lors de la récupération du code d'association :", code?.message || code?.error || code?.statusCode || code)
+
+	// On affiche le code d'association en gras
+	console.log("Code d'association : ", "\x1b[1m%s\x1b[0m", `${code.code}`)
+	console.log("Veuillez vous rendre sur Telegram et envoyer un message contenant ce code pour terminer le processus d'association.")
 }
-main().catch((err) => console.error(err));
+main()
